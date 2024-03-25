cbf2a3d534f3c43d924beaa2bebf0956fcff4391
Code-MonkeyZhang <yufengzhang483@gmail.com>
2024-03-25 10:53:05 -0400

    change forward backward speed

--- application/chassis_task.h
+++ application/chassis_task.h
@@ -102,12 +102,12 @@
 #define MAX_WHEEL_SPEED 4.0f
 //chassis forward or back max speed
 //底盘运动过程最大前进速度
-#define NORMAL_MAX_CHASSIS_SPEED_X 2.0f
+#define NORMAL_MAX_CHASSIS_SPEED_X 2.5f // 2.0f
 //chassis left or right max speed
 //底盘运动过程最大平移速度
-#define NORMAL_MAX_CHASSIS_SPEED_Y 1.5f
+#define NORMAL_MAX_CHASSIS_SPEED_Y 2.0f // 1.5f
 
-#define CHASSIS_WZ_SET_SCALE 0.1f
+#define CHASSIS_WZ_SET_SCALE 0.0f
 
 //when chassis is not set to move, swing max angle
 //摇摆原地不动摇摆最大角度(rad)
